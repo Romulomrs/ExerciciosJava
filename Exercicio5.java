4cc5648f3d666b912ba34d096e1a0f590b800b74
Rômulo dos Santos Mendes - 822161394 <romulo.gamer.br@gmail.com>
2025-03-13 21:35:49 -0300

    Exercicio 5

--- Exercicio5.java
+++ Exercicio5.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class Exercicio5 {
-    
+    public static void main(String[] args) {
+        Scanner numero = new Scanner(System.in);
+        Float Valor;
+        
+        System.out.println("Digite um número");
+        Valor = numero.nextFloat();
+        try{
+        if(Valor%2 == 1 ){
+            System.out.println("O número é ímpar");
+        }else{
+            System.out.println("O número é par");
+        }
+    }catch(Exception e){
+        System.out.println("Ocorreu um erro: " + e.getMessage());
+    }finally{
+        numero.close();
+    }
+    }
 }
