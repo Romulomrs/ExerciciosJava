59fa28b5003ecc55f023b1b4ece4205f51f33160
Rômulo dos Santos Mendes - 822161394 <romulo.gamer.br@gmail.com>
2025-03-13 21:08:15 -0300

    Terceiro exercicio

--- Exercicio3.java
+++ Exercicio3.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
+
 public class Exercicio3 {
-    
+
+    public static void main(String args[]) {
+        Scanner valor = new Scanner(System.in);
+        Double Valor1, Valor2;
+
+        try {
+            System.out.println("Digite o primeiro valor");
+            Valor1 = valor.nextDouble();
+
+            System.out.println("Digite o segundo valor");
+            Valor2 = valor.nextDouble();
+
+            // Comparando os valores
+            if (Valor1 > Valor2) {
+                System.out.println("O maior valor é: " + Valor1);  // Exibe exatamente o valor como foi inserido
+            } else if (Valor2 > Valor1) {
+                System.out.println("O maior valor é: " + Valor2);  // Exibe exatamente o valor como foi inserido
+            } else {
+                System.out.println("Valores iguais");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro: " + e.getMessage());
+        } finally {
+            valor.close();
+        }
+    }
 }
